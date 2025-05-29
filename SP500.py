c227a546f958d48b903f5aecae7a6711f64f8ef9
anthonypaulbalducci <34378880+anthonypaulbalducci@users.noreply.github.com>
2025-05-28 22:27:46 -0400

    Updated libraries and response code to pull from Wiki.

--- SP500.py
+++ SP500.py
@@ -1,7 +1,21 @@
 from bs4 import BeautifulSoup
 import yfinance as yf
 import pandas as pd
+import requests
 
 dat = yf.Ticker("MSFT")
 dat.info
-print(dat.history(period='1d'))
+print(dat.history(period='1d').Open)
+
+url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+response = requests.get(url)
+
+if response.status_code == 200:
+    content = response.text
+    print(content)
+else:
+    print("Failed to retrieve the page.")
+
+soup = BeautifulSoup(content, 'lxml')
+table = soup.find('table')
+
